701098f59fd431158a3fc3c667465d716dce00d8
Guilherme Braga <bbraga.guilherme@gmail.com>
2026-02-17 15:01:05 -0300

    fix: :bug: fix: camel_case linte

--- src/test/models/evento.test.js
+++ src/test/models/evento.test.js
@@ -1,22 +1,21 @@
 import {
-    describe, expect, it
+  describe, expect, it
 } from '@jest/globals';
 import Evento from '../../models/evento.js';
 
 describe('Testando o modelo Evento', () => {
-    const objetoEvento = {
-        nome: 'Evento teste',
-        descricao: 'descrição do evento test',
-        data: '2026-03-10',
-        autor_id: 1
-    };
+  const objetoEvento = {
+    nome: 'Evento teste',
+    descricao: 'descrição do evento test',
+    data: '2026-03-10',
+    autor_id: 1,
+  };
 
-    it('Deve instanciar um novo Evento', () => {
-        const evento = new Evento(objetoEvento);
+  it('Deve instanciar um novo Evento', () => {
+    const evento = new Evento(objetoEvento);
 
-        expect(evento).toEqual(
-            expect.objectContaining(objetoEvento),
-        );
-    });
-
+    expect(evento).toEqual(
+      expect.objectContaining(objetoEvento),
+    );
+  });
 });
